b3beeaa04ec9927b7347eb8edf8ba9453fc6354c
Jianyang <jyang95@hotmail.com>
2021-06-16 16:08:12 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -94,14 +94,14 @@ def about(update, context):
     reply_markup = InlineKeyboardMarkup(main_keyboard)
 
     message_1 = """Thank you for using Stonk Bot. Here are the key features:
-        \n\u2022 Get Latest Price (and % Change): obtain the latest price and 1-day percentage change of any US-listed stock, including those in your watchlist
+        \n\u2022 Get Latest Price (and % Change): obtain the latest price and 1-day % price change of any US-listed stock, including those in your watchlist
         \n\u2022 Get Latest News: obtain the latest popular news of any US-listed stock, including those in your watchlist
         \n\u2022 Watchlist: view, add or delete any US-listed stock. For Notification and Daily Summary
-        \n\u2022 Technical Analysis: use various popular TA tools and graphs to help in your decision making
+        \n\u2022 Technical Analysis: use various popular technical indicators and their graphs to help in your decision making
         """
     message_2 = """Autonomous features (available only if you have a watchlist):
-        \n\u2022 Daily Summary: provides the closing price and 1-day price change for a US-listed stock at the end of day (at 4am SGT) (and maybe some snark remarks)
-        \n\u2022 Notification: provides an alert on significant TA-based changes for a US-listed stock. E.g. if the stock is overbought or oversold based on the RSI (at 12am and 4am SGT). Intra-day (5-min interval) RSI notification is also now available
+        \n\u2022 Daily Summary: provides the closing price and 1-day % price change for a US-listed stock at the end of day (at 4pm ET) (and maybe some snark remarks)
+        \n\u2022 Notification: provides an alert on significant movement(s) based on the available technical indicators for a US-listed stock. E.g. if the stock is overbought or oversold based on the daily RSI (at 12pm and 4pm ET). Intraday (5-min interval) RSI notification is also now available.
         """
     message_3 = """Current version - 2.1.
         \nUpdates:
